7d7be03e9128d146852778f0aeead3e6b0756aae
esauve-csdl <80712004+esauve-csdl@users.noreply.github.com>
2022-10-26 08:36:51 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+/**
+ * Paramètres d'initialisation
+ */
+/**
+ * Affichage sur l'écran Oled de la date et de l'heure
+ */
+/**
+ * Inscrire ici la date et l'heure souhaité au moment où le bouton B est appuyé
+ */
 input.onButtonPressed(Button.A, function () {
     RTC_DS1307.DateTime(
     22,
@@ -8,12 +17,12 @@ input.onButtonPressed(Button.A, function () {
     1009
     )
 })
-input.onButtonPressed(Button.B, function () {
-	
-})
 led.enable(false)
 basic.pause(1000)
 OLED.init(128, 64)
+/**
+ * On ajoute des lignes vides pour remplir l'écran
+ */
 basic.forever(function () {
     OLED.writeStringNewLine("" + (RTC_DS1307.getTime(RTC_DS1307.TimeType.DAY) - 0) + "/" + RTC_DS1307.getTime(RTC_DS1307.TimeType.MONTH) + "/" + (RTC_DS1307.getTime(RTC_DS1307.TimeType.YEAR) - 0) + "  " + (RTC_DS1307.getTime(RTC_DS1307.TimeType.HOUR) - 0) + ":" + RTC_DS1307.getTime(RTC_DS1307.TimeType.MINUTE) + ":" + RTC_DS1307.getTime(RTC_DS1307.TimeType.SECOND) + " ")
     for (let index = 0; index < 7; index++) {
